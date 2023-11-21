49bdfd88246fef9a2266d4f99e337bcb4fdbdf78
Nikolay1992167 <wulik1081@mail.ru>
2023-11-21 17:55:32 +0300

    feat: add 6-10 tasks

--- src/main/resources/sql-task.sql
+++ src/main/resources/sql-task.sql
@@ -8,9 +8,9 @@
 
 2.SELECT model ->> 'ru' AS model,
          count(seat_no) AS quantity_of_seats
-  FROM aircrafts_data
-           JOIN seats ON aircrafts_data.aircraft_code = seats.aircraft_code
-  GROUP BY aircrafts_data.aircraft_code
+  FROM aircrafts_data AS ad
+           JOIN seats ON ad.aircraft_code = seats.aircraft_code
+  GROUP BY ad.aircraft_code
   ORDER BY quantity_of_seats DESC
   LIMIT 3;
 
@@ -30,3 +30,39 @@
    LEFT JOIN ticket_flights AS tf ON f.flight_id = tf.flight_id
        AND tf.fare_conditions = 'Business'
    WHERE tf.ticket_no IS NULL;
+
+6. SELECT DISTINCT ad.airport_name, ad.city
+   FROM airports_data AS ad
+   JOIN flights AS f ON (ad.airport_code = f.departure_airport OR ad.airport_code = f.arrival_airport)
+   WHERE (f.actual_departure > f.scheduled_departure) OR (f.actual_arrival > f.scheduled_arrival);
+
+7. SELECT ad.airport_name ->> 'ru', COUNT(f.flight_id) AS number_flights
+   FROM airports_data AS ad
+   JOIN flights AS f ON ad.airport_code = f.departure_airport
+   GROUP BY ad.airport_name
+   ORDER BY number_flights DESC;
+
+8. SELECT *
+   FROM flights AS f
+   WHERE (f.scheduled_arrival != f.actual_arrival);
+
+9. SELECT ad.aircraft_code,
+          model ->> 'ru' AS model_aircraft,
+          seat_no
+   FROM aircrafts_data AS ad
+            JOIN seats AS s ON ad.aircraft_code = s.aircraft_code
+   WHERE model ->> 'ru' = 'Аэробус A321-200'
+     AND fare_conditions != 'Economy'
+   ORDER BY seat_no;
+
+10. SELECT airport_code,
+           airport_name ->> 'ru' AS name,
+           city ->> 'ru'         AS city
+    FROM airports_data
+    WHERE city IN (SELECT city
+                   FROM airports_data
+                   GROUP BY city
+                   HAVING count(*) > 1)
+    ORDER BY city;
+
+11.
